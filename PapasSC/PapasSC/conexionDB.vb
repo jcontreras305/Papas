5d11907be9513be3e232285a023daf37bc0f94af
Isaac <ilumfinity2114@gmail.com>
2019-07-26 11:38:27 -0500

    Clase conexion

---
+++ PapasSC/PapasSC/conexionDB.vb
@@ -0,0 +1,33 @@
+﻿Imports System.Data.SqlClient
+
+Public Class conexionDB
+
+    Public conn As SqlConnection
+        Public Function conectar()
+            Try
+            conn = New SqlConnection("data source=localhost; initial catalog=PapaSC; integrated security=true")
+            'conn = New SqlConnection("data source=localhost; initial catalog=PapaSC; integrated security=true") 'aqui cambia el nombre de su servidor Larigza
+            'conn = New SqlConnection("data source=localhost; initial catalog=PapaSC; integrated security=true") 'aqui cambia el nombre de su servidor Chuy
+            conn.Open()
+                If conn.State Then
+                    MsgBox("la conexion esta abierta")
+                End If
+  
+            Catch ex As Exception
+                MsgBox(ex.Message)
+            End Try
+            Return True
+        End Function
+
+        Public Function desconectar()
+            Try
+                If (conn.State = ConnectionState.Open) Then
+                    conn.Close()
+                End If
+            Catch ex As Exception
+                MsgBox(ex.Message)
+            End Try
+            Return False
+        End Function
+
+    End Class
