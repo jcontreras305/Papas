24957dc9e872fbbe0a9804fd7a36977b2796baca
Isaac <ilumfinity2114@gmail.com>
2019-08-01 10:42:13 -0500

    inventario

---
+++ PapasSC/PapasSC/Formularios/Invertario.Designer.vb
@@ -0,0 +1,113 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Invertario
+    Inherits System.Windows.Forms.Form
+
+    'Form reemplaza a Dispose para limpiar la lista de componentes.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Requerido por el Diseñador de Windows Forms
+    Private components As System.ComponentModel.IContainer
+
+    'NOTA: el Diseñador de Windows Forms necesita el siguiente procedimiento
+    'Se puede modificar usando el Diseñador de Windows Forms.  
+    'No lo modifique con el editor de código.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        Me.cmbBodega = New System.Windows.Forms.ComboBox()
+        Me.cmbProducto = New System.Windows.Forms.ComboBox()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.CheckBox1 = New System.Windows.Forms.CheckBox()
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.SuspendLayout()
+        '
+        'DataGridView1
+        '
+        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.DataGridView1.Location = New System.Drawing.Point(25, 124)
+        Me.DataGridView1.Name = "DataGridView1"
+        Me.DataGridView1.Size = New System.Drawing.Size(619, 237)
+        Me.DataGridView1.TabIndex = 0
+        '
+        'cmbBodega
+        '
+        Me.cmbBodega.FormattingEnabled = True
+        Me.cmbBodega.Location = New System.Drawing.Point(103, 32)
+        Me.cmbBodega.Name = "cmbBodega"
+        Me.cmbBodega.Size = New System.Drawing.Size(153, 21)
+        Me.cmbBodega.TabIndex = 1
+        Me.cmbBodega.Text = "--"
+        '
+        'cmbProducto
+        '
+        Me.cmbProducto.FormattingEnabled = True
+        Me.cmbProducto.Location = New System.Drawing.Point(103, 78)
+        Me.cmbProducto.Name = "cmbProducto"
+        Me.cmbProducto.Size = New System.Drawing.Size(153, 21)
+        Me.cmbProducto.TabIndex = 2
+        Me.cmbProducto.Text = "--"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(30, 40)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(44, 13)
+        Me.Label2.TabIndex = 4
+        Me.Label2.Text = "Bodega"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(30, 80)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(50, 13)
+        Me.Label3.TabIndex = 5
+        Me.Label3.Text = "Producto"
+        '
+        'CheckBox1
+        '
+        Me.CheckBox1.AutoSize = True
+        Me.CheckBox1.Location = New System.Drawing.Point(479, 82)
+        Me.CheckBox1.Name = "CheckBox1"
+        Me.CheckBox1.Size = New System.Drawing.Size(81, 17)
+        Me.CheckBox1.TabIndex = 6
+        Me.CheckBox1.Text = "CheckBox1"
+        Me.CheckBox1.UseVisualStyleBackColor = True
+        '
+        'Invertario
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(678, 458)
+        Me.Controls.Add(Me.CheckBox1)
+        Me.Controls.Add(Me.Label3)
+        Me.Controls.Add(Me.Label2)
+        Me.Controls.Add(Me.cmbProducto)
+        Me.Controls.Add(Me.cmbBodega)
+        Me.Controls.Add(Me.DataGridView1)
+        Me.Name = "Invertario"
+        Me.Text = "Invertario"
+        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents DataGridView1 As DataGridView
+    Friend WithEvents cmbBodega As ComboBox
+    Friend WithEvents cmbProducto As ComboBox
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents CheckBox1 As CheckBox
+End Class
---
+++ PapasSC/PapasSC/Metodos/MetodosInventario.vb
@@ -0,0 +1,24 @@
+﻿Imports System.Data.SqlClient
+
+Public Class MetodosInventario
+    Inherits conexionDB
+
+    Public Sub buscarBodegas(ByVal cmbBodega As ComboBox)
+        Try
+            conectar()
+            Dim lista As New List(Of String)
+            Dim comando = New SqlCommand("sp_selectBodega")
+            comando.Connection = conn
+            comando.CommandType = CommandType.StoredProcedure
+            Dim reader = comando.ExecuteReader()
+            While reader.Read
+                lista.Add(CStr(reader("bodega")))
+            End While
+            cmbBodega.DataSource = lista
+        Catch ex As Exception
+
+        End Try
+
+    End Sub
+
+End Class
