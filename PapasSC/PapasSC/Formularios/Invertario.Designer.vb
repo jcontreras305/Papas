2caac6fbc13802311311eb43ab8e2c09776cb9d8
Isaac <ilumfinity2114@gmail.com>
2019-08-01 13:11:55 -0500

    inventario1.1

--- PapasSC/PapasSC/Formularios/Invertario.Designer.vb
+++ PapasSC/PapasSC/Formularios/Invertario.Designer.vb
@@ -22,27 +22,48 @@ Partial Class Invertario
     'No lo modifique con el editor de código.
     <System.Diagnostics.DebuggerStepThrough()> _
     Private Sub InitializeComponent()
-        Me.DataGridView1 = New System.Windows.Forms.DataGridView()
+        Dim DataGridViewCellStyle5 As System.Windows.Forms.DataGridViewCellStyle = New System.Windows.Forms.DataGridViewCellStyle()
+        Dim DataGridViewCellStyle6 As System.Windows.Forms.DataGridViewCellStyle = New System.Windows.Forms.DataGridViewCellStyle()
+        Me.tblInventario = New System.Windows.Forms.DataGridView()
         Me.cmbBodega = New System.Windows.Forms.ComboBox()
         Me.cmbProducto = New System.Windows.Forms.ComboBox()
         Me.Label2 = New System.Windows.Forms.Label()
         Me.Label3 = New System.Windows.Forms.Label()
-        Me.CheckBox1 = New System.Windows.Forms.CheckBox()
-        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).BeginInit()
+        Me.chbExistencia = New System.Windows.Forms.CheckBox()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.Label4 = New System.Windows.Forms.Label()
+        CType(Me.tblInventario, System.ComponentModel.ISupportInitialize).BeginInit()
         Me.SuspendLayout()
         '
-        'DataGridView1
+        'tblInventario
         '
-        Me.DataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
-        Me.DataGridView1.Location = New System.Drawing.Point(25, 124)
-        Me.DataGridView1.Name = "DataGridView1"
-        Me.DataGridView1.Size = New System.Drawing.Size(619, 237)
-        Me.DataGridView1.TabIndex = 0
+        DataGridViewCellStyle5.Padding = New System.Windows.Forms.Padding(20, 0, 0, 0)
+        Me.tblInventario.AlternatingRowsDefaultCellStyle = DataGridViewCellStyle5
+        Me.tblInventario.BackgroundColor = System.Drawing.SystemColors.ActiveBorder
+        Me.tblInventario.BorderStyle = System.Windows.Forms.BorderStyle.None
+        DataGridViewCellStyle6.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft
+        DataGridViewCellStyle6.BackColor = System.Drawing.SystemColors.Control
+        DataGridViewCellStyle6.Font = New System.Drawing.Font("Microsoft Sans Serif", 10.0!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        DataGridViewCellStyle6.ForeColor = System.Drawing.SystemColors.WindowText
+        DataGridViewCellStyle6.SelectionBackColor = System.Drawing.SystemColors.Highlight
+        DataGridViewCellStyle6.SelectionForeColor = System.Drawing.SystemColors.HighlightText
+        DataGridViewCellStyle6.WrapMode = System.Windows.Forms.DataGridViewTriState.[True]
+        Me.tblInventario.ColumnHeadersDefaultCellStyle = DataGridViewCellStyle6
+        Me.tblInventario.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize
+        Me.tblInventario.EnableHeadersVisualStyles = False
+        Me.tblInventario.GridColor = System.Drawing.SystemColors.MenuHighlight
+        Me.tblInventario.Location = New System.Drawing.Point(30, 148)
+        Me.tblInventario.Name = "tblInventario"
+        Me.tblInventario.RowHeadersVisible = False
+        Me.tblInventario.SelectionMode = System.Windows.Forms.DataGridViewSelectionMode.FullRowSelect
+        Me.tblInventario.Size = New System.Drawing.Size(619, 237)
+        Me.tblInventario.TabIndex = 0
         '
         'cmbBodega
         '
         Me.cmbBodega.FormattingEnabled = True
-        Me.cmbBodega.Location = New System.Drawing.Point(103, 32)
+        Me.cmbBodega.Location = New System.Drawing.Point(108, 56)
         Me.cmbBodega.Name = "cmbBodega"
         Me.cmbBodega.Size = New System.Drawing.Size(153, 21)
         Me.cmbBodega.TabIndex = 1
@@ -51,7 +72,7 @@ Partial Class Invertario
         'cmbProducto
         '
         Me.cmbProducto.FormattingEnabled = True
-        Me.cmbProducto.Location = New System.Drawing.Point(103, 78)
+        Me.cmbProducto.Location = New System.Drawing.Point(108, 102)
         Me.cmbProducto.Name = "cmbProducto"
         Me.cmbProducto.Size = New System.Drawing.Size(153, 21)
         Me.cmbProducto.TabIndex = 2
@@ -60,7 +81,7 @@ Partial Class Invertario
         'Label2
         '
         Me.Label2.AutoSize = True
-        Me.Label2.Location = New System.Drawing.Point(30, 40)
+        Me.Label2.Location = New System.Drawing.Point(35, 64)
         Me.Label2.Name = "Label2"
         Me.Label2.Size = New System.Drawing.Size(44, 13)
         Me.Label2.TabIndex = 4
@@ -69,45 +90,83 @@ Partial Class Invertario
         'Label3
         '
         Me.Label3.AutoSize = True
-        Me.Label3.Location = New System.Drawing.Point(30, 80)
+        Me.Label3.Location = New System.Drawing.Point(35, 104)
         Me.Label3.Name = "Label3"
         Me.Label3.Size = New System.Drawing.Size(50, 13)
         Me.Label3.TabIndex = 5
         Me.Label3.Text = "Producto"
         '
-        'CheckBox1
+        'chbExistencia
         '
-        Me.CheckBox1.AutoSize = True
-        Me.CheckBox1.Location = New System.Drawing.Point(479, 82)
-        Me.CheckBox1.Name = "CheckBox1"
-        Me.CheckBox1.Size = New System.Drawing.Size(81, 17)
-        Me.CheckBox1.TabIndex = 6
-        Me.CheckBox1.Text = "CheckBox1"
-        Me.CheckBox1.UseVisualStyleBackColor = True
+        Me.chbExistencia.AutoSize = True
+        Me.chbExistencia.Location = New System.Drawing.Point(510, 106)
+        Me.chbExistencia.Name = "chbExistencia"
+        Me.chbExistencia.Size = New System.Drawing.Size(118, 17)
+        Me.chbExistencia.TabIndex = 6
+        Me.chbExistencia.Text = "Solo con existencia"
+        Me.chbExistencia.UseVisualStyleBackColor = True
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(492, 391)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(131, 46)
+        Me.Button1.TabIndex = 7
+        Me.Button1.Text = "Button1"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Font = New System.Drawing.Font("Arial", 15.75!, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label1.ForeColor = System.Drawing.SystemColors.ButtonShadow
+        Me.Label1.Location = New System.Drawing.Point(2, 3)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(24, 24)
+        Me.Label1.TabIndex = 10
+        Me.Label1.Text = "X"
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Font = New System.Drawing.Font("Arial", 15.75!, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, CType(0, Byte))
+        Me.Label4.ForeColor = System.Drawing.SystemColors.ButtonShadow
+        Me.Label4.Location = New System.Drawing.Point(265, 9)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(111, 24)
+        Me.Label4.TabIndex = 9
+        Me.Label4.Text = "Inventario"
         '
         'Invertario
         '
         Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
         Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
         Me.ClientSize = New System.Drawing.Size(678, 458)
-        Me.Controls.Add(Me.CheckBox1)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.Label4)
+        Me.Controls.Add(Me.Button1)
+        Me.Controls.Add(Me.chbExistencia)
         Me.Controls.Add(Me.Label3)
         Me.Controls.Add(Me.Label2)
         Me.Controls.Add(Me.cmbProducto)
         Me.Controls.Add(Me.cmbBodega)
-        Me.Controls.Add(Me.DataGridView1)
+        Me.Controls.Add(Me.tblInventario)
+        Me.FormBorderStyle = System.Windows.Forms.FormBorderStyle.None
         Me.Name = "Invertario"
         Me.Text = "Invertario"
-        CType(Me.DataGridView1, System.ComponentModel.ISupportInitialize).EndInit()
+        CType(Me.tblInventario, System.ComponentModel.ISupportInitialize).EndInit()
         Me.ResumeLayout(False)
         Me.PerformLayout()
 
     End Sub
 
-    Friend WithEvents DataGridView1 As DataGridView
+    Friend WithEvents tblInventario As DataGridView
     Friend WithEvents cmbBodega As ComboBox
     Friend WithEvents cmbProducto As ComboBox
     Friend WithEvents Label2 As Label
     Friend WithEvents Label3 As Label
-    Friend WithEvents CheckBox1 As CheckBox
+    Friend WithEvents chbExistencia As CheckBox
+    Friend WithEvents Button1 As Button
+    Friend WithEvents Label1 As Label
+    Friend WithEvents Label4 As Label
 End Class
