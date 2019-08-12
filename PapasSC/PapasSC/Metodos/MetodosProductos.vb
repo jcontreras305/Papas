0d960ff8e7e7e0555ab9c533ccb79fa226f4fa42
Larigza Chávez Fierro <larigza19971201@hotmail.com>
2019-08-12 13:08:58 -0500

    imag

---
+++ PapasSC/PapasSC/Metodos/MetodosProductos.vb
@@ -0,0 +1,27 @@
+﻿Imports System.Data.SqlClient
+
+Public Class MetodosProductos
+    Inherits conexionDB
+    Dim cmd As New SqlCommand
+    Public adaptador As SqlDataAdapter
+
+    Public Function InsertarProducto(ver As String, cla As String)
+        Try
+            conectar()
+            cmd = New SqlCommand("sp_Insertar_Producto")
+            cmd.CommandType = CommandType.StoredProcedure
+            cmd.Connection = conn
+
+            cmd.Parameters.AddWithValue("@version", ver)
+            cmd.Parameters.AddWithValue("@clave", cla)
+            If cmd.ExecuteNonQuery Then
+                Return True
+            End If
+        Catch ex As Exception
+            MsgBox(ex.Message)
+        Return Nothing
+        End Try
+
+    End Function
+
+End Class
