94bf47463d66bf006c91c336b9441aed374360cc
Isaac <ilumfinity2114@gmail.com>
2019-08-13 12:13:41 -0500

    Matriz Cambio

---
+++ PapasSC/PapasSC/Formularios/Matriz.vb
@@ -0,0 +1,12 @@
+﻿Imports PapasSC.MetodosMatrizCiudad
+Public Class Matriz
+    Dim mtdMatCD As New MetodosMatrizCiudad
+
+    Private Sub Matriz_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Try
+            mtdMatCD.seleccionar_Matriz(tblMatriz)
+        Catch ex As Exception
+            MsgBox(ex.Message)
+        End Try
+    End Sub
+End Class
---
+++ PapasSC/PapasSC/Metodos/MetodosMatrizCiudad.vb
@@ -0,0 +1,22 @@
+﻿Imports System.Data.SqlClient
+
+Public Class MetodosMatrizCiudad
+    Inherits conexionDB
+
+    Public Sub seleccionar_Matriz(ByVal tablaMatriz As DataGridView)
+        Try
+            conectar()
+            Dim cmd = New SqlCommand("sp_select_Matriz")
+            cmd.CommandType = CommandType.StoredProcedure
+            cmd.Connection = conn
+            If cmd.ExecuteNonQuery Then
+                Dim dt As New DataTable
+                Dim da As New SqlDataAdapter(cmd)
+                da.Fill(dt)
+                tablaMatriz.DataSource = dt
+            End If
+        Catch ex As Exception
+            MsgBox(ex.Message)
+        End Try
+    End Sub
+End Class
