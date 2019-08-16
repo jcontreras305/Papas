bb2cd883834f963ea335cca238c45ddbd6025b17
Isaac <ilumfinity2114@gmail.com>
2019-08-16 12:28:31 -0500

    Impresoras Todas la impresoras y atributos

--- PapasSC/PapasSC/Formularios/MenuConfiguracion.vb
+++ PapasSC/PapasSC/Formularios/MenuConfiguracion.vb
@@ -18,4 +18,8 @@
     Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
         Matriz.Show()
     End Sub
+
+    Private Sub Button3_Click(sender As Object, e As EventArgs) Handles Button3.Click
+        Impresoras.Show()
+    End Sub
 End Class
