fe811ee9649a5637950f9cd7fd5d874d96956e1d
Isaac <ilumfinity2114@gmail.com>
2019-11-06 12:54:18 -0600

    error en boton agregar

--- PapasSC/PapasSC/My Project/Resources.Designer.vb
+++ PapasSC/PapasSC/My Project/Resources.Designer.vb
@@ -83,6 +83,16 @@ Namespace My.Resources
         '''<summary>
         '''  Busca un recurso adaptado de tipo System.Drawing.Bitmap.
         '''</summary>
+        Friend ReadOnly Property agregar() As System.Drawing.Bitmap
+            Get
+                Dim obj As Object = ResourceManager.GetObject("agregar", resourceCulture)
+                Return CType(obj,System.Drawing.Bitmap)
+            End Get
+        End Property
+        
+        '''<summary>
+        '''  Busca un recurso adaptado de tipo System.Drawing.Bitmap.
+        '''</summary>
         Friend ReadOnly Property BILLETE() As System.Drawing.Bitmap
             Get
                 Dim obj As Object = ResourceManager.GetObject("BILLETE", resourceCulture)
